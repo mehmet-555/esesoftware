d544e45dc8e03f5e79157d5d84f0c254aad87b0a
Mehmet Güngör <gungormm321@hotmail.com>
2024-05-31 06:09:52 +0300

    Navigation bar bitti

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
       fontFamily: {
         mono: ['Roboto Mono', 'monospace', 'sans-serif'],
       },
+      backgroundImage: {
+        'initial-gradient': 'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%)',
+        'hover-gradient': 'linear-gradient(133deg, rgba(33,62,242,1) 0%, rgba(255,0,0,1) 100%)'
+      }
     },
   },
   plugins: [],
