c2838edeb87cbbc24f87a2af4ad06070797ea9f0
Mehmet Güngör <gungormm321@hotmail.com>
2024-06-03 16:30:45 +0300

    SSS Bölümü üzerinde çalışılmaya başlandı.

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Contact Form Style Management
+    // Contact Form Style Management Başlangıç
     const inputs = document.querySelectorAll(".contactFormT input, .contactFormT textarea");
     const setLabelStyle = (label, styles) => {
         Object.assign(label.style, styles);
@@ -27,10 +27,42 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
+    // Contact Form Style Management Son
+
+
+
+
+    // Sık Sorulan Sorulan (SSS) Başlangıç
+
+    //  {Büyük Butonlar}
+    const sssBigBtns = document.querySelectorAll(".sssBigBtn");
+    sssBigBtns.forEach(sssBigBtn => {
+        sssBigBtn.addEventListener("click", (e)=> {
+            e.target.closest("li").style.padding = "1rem";
+            e.target.closest("li").querySelector(".hidden").classList.remove("hidden")
+        })
+    })
+
+    //  {Küçük Butonlar}
+    const sssSmBtns = document.querySelectorAll(".sssSmBtn");
+    sssSmBtns.forEach(sssSmBtn => {
+        sssSmBtn.addEventListener("click", (e)=> {
+            const closestUl = e.target.closest("ul");
+            const otherBtns = closestUl.querySelectorAll(".sssSmBtn");
+            otherBtns.forEach(otherBtn => {
+                if(e.target !== otherBtn) {
+                    otherBtn.click();
+                }
+            })
+            e.target.closest("li").querySelector(".pCntr").classList.contains("hidden") ? e.target.closest("li").querySelector(".pCntr").classList.remove("hidden") : e.target.closest("li").querySelector(".pCntr").classList.add("hidden"); 
+        })
+    })
+    // Sık Sorulan Sorulan (SSS) Son
+
 });
 
 
-
+// Sayfada Kaldığım Yer Başlangıç
 window.addEventListener("beforeunload", function() {
     localStorage.setItem("scrollPosition", document.querySelector(".mainCustomheigth").scrollTop);
 });
@@ -45,3 +77,4 @@ window.addEventListener("load", function() {
         document.querySelector(".mainCustomheigth").scrollTop = parseInt(scrollPosition, 10);
     }
 });
+// Sayfada Kaldığım Yer Son
