2620164e23968697508ca030c96226a79cc70b0a
Mehmet Güngör <gungormm321@hotmail.com>
2024-07-12 11:41:40 +0300

    Form verilerini almak için açılan brnachde servisler ve hizmetler sayfasına kadar gelindi.

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ const rootDir = require("./utils/rootDir");
 const homeRouter = require("./routes/homeRouters");
 const projectRouter = require("./routes/projectRouters");
 const aboutRouter = require("./routes/aboutRouters");
+const servicesRouter = require("./routes/servicesRouters.js");
 
 // App
 const app = express();
 
 // Internal Middlewares
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.set("views", path.join(rootDir, "views"));
 app.use(express.static(path.join(rootDir, "/public")));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
+app.use("/services", servicesRouter);
 app.use("/project", projectRouter);
 app.use("/about", aboutRouter);
 app.use("/", homeRouter);
---
+++ routes/servicesRouters.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+
+const servicesRouter = express.Router();
+
+const webSRouter = express.Router();
+const automationSRouter = express.Router();
+const gcpSRouter = express.Router();
+const corporateSRouter = express.Router();
+const scrapingSRouter = express.Router();
+
+
+
+
+
+
+
+servicesRouter.use("/web-services", webSRouter);
+servicesRouter.use("/", (req, res, next) => {
+    res.status(200).render("services/services.ejs");
+});
+
+
+
+
+
+webSRouter.get("/creating-static-web-site", (req, res, next) => {
+    res.status(200).render("services/webServices/creatingStaticWebSite");
+})
+
+// webSRouter.get("/", (req, res, next) => {
+//     // res.status(200).render("services/webServices/webServices");
+// });
+
+
+
+ 
+
+
+module.exports = servicesRouter;
