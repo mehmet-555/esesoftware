70892b0e3ef4aa530c5d4495a554db65af893523
Mehmet Güngör <gungormm321@hotmail.com>
2024-08-08 14:44:10 +0300

    index.html index.js olarak değiştirildi. ve contact form çerez ayaraları yapılıyor

--- routes/homeRouters.js
+++ routes/homeRouters.js
@@ -6,14 +6,19 @@ const express = require("express");
 const router = express.Router();
 
 const rootDir = require("../utils/rootDir");
+const { route } = require("./homeRouters");
 
 const secretKey = process.env.ESESOFTWARE_SECRET_KEY_FOR_CONTACT_BOX;
 
 router.use(cookieParser());
 
-router.get("/", (req, res, next)=> {
-    res.sendFile(path.join(__dirname, "../public/pages/index.html"));
-});
+router.get("/", checkSignedCookie, (req, res, next) => {
+    console.log("ÇLAIŞTI 4")
+    res.status(200).render("staticContents/index", {
+        sendMessage: true
+    })
+    console.log("ÇLAIŞTI 5")
+})
 router.post("/contact", (req, res, next) => {
     console.log(req.body)
     const messageSend = {
@@ -22,9 +27,36 @@ router.post("/contact", (req, res, next) => {
     // JWT Oluşturma
     const sendMessageJWT = jwt.sign(messageSend, secretKey, { expiresIn: '1d' });
 
-    res.cookie("ES_smCookie", sendMessageJWT, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.cookie("ES_smCookie", sendMessageJWT, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000, secure: true });
     res.status(200).send("Message received and cookie send")
 });
 
+
+
+
+
+// İmzalı çerezi kontrol eden middleware
+function checkSignedCookie(req, res, next) {
+    const ES_smCookie = req.cookies.ES_smCookie;
+    console.log(ES_smCookie)
+    if (!ES_smCookie) {
+        console.log("ÇLAIŞTI 3")
+        return res.status(200).render("staticContents/index", {
+            sendMessage: false
+        })
+    }
+
+    try {
+        const verified = jwt.verify(ES_smCookie, secretKey);
+        next();
+        console.log("ÇLAIŞTI 1")
+    } catch (err) {
+        console.log("ÇLAIŞTI 2")
+        return res.status(200).render("staticContents/index", {
+            sendMessage: false
+        });
+        
+    }
+}
 
 module.exports = router;
