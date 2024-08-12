a99aa386063113ba1da40c0b4a37b7c67843a668
Mehmet Güngör <gungormm321@hotmail.com>
2024-08-12 23:20:37 +0300

    qr menü sayfası eklendi.

--- routes/servicesRouters.js
+++ routes/servicesRouters.js
@@ -268,7 +268,34 @@ corporateRouter.get("/corparate-intranet-solutions", [
     }else {
         return res.status(400).json({ errors: errors.array() });
     };
-})
+});
+corporateRouter.get("/qr-menu-application", [
+    // query('url').isURL().withMessage('Must be a valid URL')   // Daha sonra aktif edeceğim!!!
+], async (req, res, next) => { 
+    
+    // console.log(req.originalUrl);
+    const errors = validationResult(req);
+
+    if(errors.isEmpty()) {
+        try {
+            const thePathArr = req.originalUrl.split("/").slice(1).map(part => validator.escape(part));
+            const pagesJson = await fs.readFile(path.join(rootDir, "model/pages/sPages.json"), 'utf8');
+            const pageObj = JSON.parse(pagesJson);
+            // console.log(pageObj);
+            // console.log(req.ip);
+            
+            res.status(200).render('services/webServices/servicesGeneralLayout', {
+                thePathArr: thePathArr,
+                page: pageObj.pages[8],
+                pageTitle: pageObj.pages[8].pageTitle
+            });
+        } catch (error) {
+            next(error);
+        };
+    }else {
+        return res.status(400).json({ errors: errors.array() });
+    };
+});
 module.exports = servicesRouter;
 
 
