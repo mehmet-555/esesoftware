6fd289d38cd78d812da62c21c8d3ed7e1a684b1e
Mehmet Güngör <gungormm321@hotmail.com>
2024-08-15 00:12:18 +0300

    FE js dosyalarındaki Otomasyon Çözümleri linki yazım hatası düzeltildi.

--- public/js/services.js
+++ public/js/services.js
@@ -208,7 +208,7 @@ function showContentsOnServicesOnModal(index) {
         ],
         [   
             `<li class='fade-in text-lg font-medium text-[#adadad] ml-3 tracking-wider'>Otomasyon</li>`,
-            `<li class='fade-in'><a href="Otomasyon Çözümleri" class='inline-block w-full whitespace-nowrap py-2 px-3 bg-transparent hover:bg-[#10151e] rounded-lg transition-all duration-200'><span class='text-sm text-gray-400 mr-1'>></span> Otomasyon Çözümleri</a></li>`
+            `<li class='fade-in'><a href="/services/automation/automation-solutions" class='inline-block w-full whitespace-nowrap py-2 px-3 bg-transparent hover:bg-[#10151e] rounded-lg transition-all duration-200'><span class='text-sm text-gray-400 mr-1'>></span> Otomasyon Çözümleri</a></li>`
         ],
         [   
             `<li class='fade-in text-lg font-medium text-[#adadad] ml-3 tracking-wider'>İşletme / Kurumsal</li>`,
