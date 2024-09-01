34bfcc87f2d716e2e265e0520f96d3908bfec46f
Mehmet Güngör <gungormm321@hotmail.com>
2024-09-01 16:11:20 +0300

    unsubrices.js oluşturudlu.3

--- routes/unsubscribeRouters.js
+++ routes/unsubscribeRouters.js
@@ -1,31 +1,33 @@
 const express = require("express");
-
 const router = express.Router();
 const userIsUnsubscribed = require("../model/subscribes/unsubscribe");
 
-
 router.route("/")
-    .get((req, res, next) => {
+    .get(async (req, res, next) => { // .get metodunu async olarak tanımlıyoruz
         const email = req.query.email;
         const token = req.query.token;
-        console.log("cosnole 6")
-        if(userIsUnsubscribed(email, token)) {
-            console.log("cosnole 7")
-            res.status(200).render("staticContents/unsubscribe", {
-                isUnsubscribedYet: true
-            })
-        }else {
-            console.log("cosnole 8")
-            res.status(200).render("staticContents/unsubscribe", {
-                isUnsubscribedYet: false
-            })
-        }
+        console.log("console 6");
 
+        try {
+            const isUnsubscribed = await userIsUnsubscribed(email, token); // await kullanıyoruz
+            if (isUnsubscribed) {
+                console.log("console 7");
+                res.status(200).render("staticContents/unsubscribe", {
+                    isUnsubscribedYet: true
+                });
+            } else {
+                console.log("console 8");
+                res.status(200).render("staticContents/unsubscribe", {
+                    isUnsubscribedYet: false
+                });
+            }
+        } catch (error) {
+            console.error("An error occurred: ", error);
+            res.status(500).send("Internal Server Error");
+        }
     })
     .post((req, res, next) => {
-
-    })
-
-
+        // POST işlemi burada yapılabilir
+    });
 
 module.exports = router;
